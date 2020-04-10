060c8d346f3344487a277bf9ee9d61c495d73ad8
Amit Pundir <amit.pundir@linaro.org>
2020-04-10 22:47:13 +0530

    device-common: Enable printing feature
    cherry-picked from upstream device/linaro/dragonboard project.
    
    Right now "Connection preferences" tab buried in
    "Settings" application crashes on user because
    Printing feature is not enabled for dragonboards.
    Copy Print feature (android.software.print)
    manifest to fix that.
    
    Test: From UI navigate to
          "Settings" ->
              "Connected devices" ->
                  "Connection preferences"
          and interact with BT, Cast and Printing services.
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Change-Id: If2741ef00ff3d117c6d2a98c0d2578acc8d3acfc

--- device-common.mk
+++ device-common.mk
@@ -163,6 +163,7 @@ PRODUCT_COPY_FILES +=  \
     frameworks/native/data/etc/android.software.backup.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.backup.xml \
     frameworks/native/data/etc/android.software.voice_recognizers.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.voice_recognizers.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml \
+    frameworks/native/data/etc/android.software.print.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.print.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.software.device_admin.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_admin.xml
