d9bec35a0c97b68a2ab13c9dca82694154d7de93
Sumit Semwal <sumit.semwal@linaro.org>
2020-05-15 16:05:06 +0530

    beryllium: Add fw_devlink=permissive to the boot arguments
    android-mainline commit
    ("ANDROID: GKI: driver core: Set fw_devlink=on by default"),
    https://android-review.googlesource.com/c/1305327
    broke all db845c based platforms using android-mainline.
    
    Set fw_devlink=permissive while we debug it further.
    
    Signed-off-by: Sumit Semwal <sumit.semwal@linaro.org>

--- aosp_beryllium/BoardConfig.mk
+++ aosp_beryllium/BoardConfig.mk
@@ -14,6 +14,7 @@ BOARD_KERNEL_CMDLINE     := console=ttyMSM0,115200n8 androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE     += androidboot.hardware=aosp_beryllium androidboot.boot_devices=soc@0/1d84000.ufshc
 BOARD_KERNEL_CMDLINE     += firmware_class.path=/vendor/firmware/ init=/init printk.devkmsg=on
 BOARD_KERNEL_CMDLINE     += deferred_probe_timeout=30
+BOARD_KERNEL_CMDLINE     += fw_devlink=permissive
  
 # Image Configuration
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864 #64M
