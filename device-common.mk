db705adea9b4b459ce41783d094483bff5ade48e
Amit Pundir <amit.pundir@linaro.org>
2021-03-10 17:53:38 +0530

    health: Switch to Cuttlefish' healthd HAL impl
    cherry-picked from upstream device/linaro/dragonboard project.
    
    Mostly for the fake battery stats to keep the Android
    Runtime happy.
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Change-Id: Ie08ffb2f22413532f6cc2eec71e02141aed17212

--- device-common.mk
+++ device-common.mk
@@ -157,8 +157,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-service \
-    android.hardware.health@2.0-impl
+    android.hardware.health@2.1-impl-cuttlefish \
+    android.hardware.health@2.1-service
 
 # mkbootimg host tool to build boot.img separately
 PRODUCT_HOST_PACKAGES := \
