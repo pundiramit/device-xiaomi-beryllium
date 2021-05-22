b7c457bd2f204ec3815c0fb5cc663decea10e6ff
Amit Pundir <amit.pundir@linaro.org>
2021-05-22 13:49:05 +0530

    prebuilt-kernel: Remove android-5.4 prebuilts
    Will be publishing only android-mainline and
    upstream kernel prebuilts from here onwards.
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -1,7 +1,7 @@
 ifneq ($(filter beryllium, $(TARGET_DEVICE)),)
 
 IMAGE_GZ := device/xiaomi/beryllium/prebuilt-kernel/android-$(TARGET_KERNEL_USE)/Image.gz
-DTB := device/xiaomi/beryllium/prebuilt-kernel/android-$(TARGET_KERNEL_USE)/sdm845-beryllium.dtb
+DTB := device/xiaomi/beryllium/prebuilt-kernel/android-$(TARGET_KERNEL_USE)/sdm845-xiaomi-beryllium.dtb
 
 $(PRODUCT_OUT)/kernel: $(IMAGE_GZ) $(DTB)
 	cat $(IMAGE_GZ) $(DTB) > $@
