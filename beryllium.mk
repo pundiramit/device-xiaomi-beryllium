1d6d98e3ff2e4aed24a9e2be460693d575135191
Amit Pundir <amit.pundir@linaro.org>
2021-09-27 00:09:10 +0530

    beryllium.mk: Copy kernel modules to ramdisk only
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- beryllium.mk
+++ beryllium.mk
@@ -2,15 +2,9 @@ ifndef TARGET_KERNEL_USE
 TARGET_KERNEL_USE := mainline
 endif
 
+# All kernel modules are installed in ramdisk
 KERNEL_MODS := $(wildcard device/xiaomi/beryllium/prebuilt-kernel/android-$(TARGET_KERNEL_USE)/*.ko)
-
-# Following modules go to vendor partition
-# msm.ko is too big (31M) for ramdisk
-VENDOR_KERN_MODS := %/msm.ko
-BOARD_VENDOR_KERNEL_MODULES := $(filter $(VENDOR_KERN_MODS),$(KERNEL_MODS))
-
-# All other modules go to ramdisk
-BOARD_GENERIC_RAMDISK_KERNEL_MODULES := $(filter-out $(VENDOR_KERN_MODS),$(KERNEL_MODS))
+BOARD_GENERIC_RAMDISK_KERNEL_MODULES := $(KERNEL_MODS)
 
 # Inherit the full_base and device configurations
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
